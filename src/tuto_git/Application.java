10b2f62f0a38adf97ae2bcd2858e3dc9d2cec323
Thomas Emile <temile@pcalb-mm0000.univ-jfc.fr>
2023-01-25 12:15:05 +0100

    ajoue de la sortie d'application : FERME LA FENETRE

--- src/tuto_git/Application.java
+++ src/tuto_git/Application.java
@@ -1,8 +1,10 @@
 package tuto_git;
 
 import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class Application extends Frame{
+public class Application extends Frame implements WindowListener {
 
 	public static void main(String[] args) {
 		new Application();
@@ -18,8 +20,51 @@ public class Application extends Frame{
 		vue.addActionListener(ctrl);
 		this.setTitle("tutoriel git");
 		this.add(vue);
+		this.addWindowListener(this);
 		this.pack();
 		this.setVisible(true);
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
